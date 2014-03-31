6e67d09294f79aac4c489a15f29588466a1d5884
wingsking <jz2540@columbia.edu>
2014-03-31 14:52:12 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,4 +5,3 @@ CP1="src:commons-codec-1.9.jar:htmlparser.jar:org-json.jar:"
 CP2="bin:commons-codec-1.9.jar:htmlparser.jar:org-json.jar:"
 javac -d bin/ -cp ${CP1} src/*/*java
 java -cp ${CP2}  bing.BingSearch $1 $2 "$3"
-#cnLsEsvYTSd+XBWkE4lO7z02Wgh3W14UTAwgJ/JURdc= 0.9 "apple stock"
